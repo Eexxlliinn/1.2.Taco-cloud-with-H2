523e0d6c417c7dc31435369ac5d665667e1e2c50
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-10-17 17:35:10 +0300

    Init commit

---
+++ src/main/java/com/example/tacocloudwithjpa/TacoCloudWithJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.tacocloudwithjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudWithJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudWithJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tacocloudwithjpa/TacoCloudWithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tacocloudwithjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudWithJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
