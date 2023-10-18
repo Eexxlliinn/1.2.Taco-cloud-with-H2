c7ac05cde074bbec1d5607a328f680884302ee3e
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-10-18 12:49:26 +0300

    commit

--- src/main/java/data/Ingredient.java
+++ src/main/java/data/Ingredient.java
@@ -1,9 +1,16 @@
 package data;
 
-import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
 
 @Data
+@Entity
+@AllArgsConstructor
+@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
 public class Ingredient {
+    @Id
     private final String id;
     private final String name;
     private final Type type;
