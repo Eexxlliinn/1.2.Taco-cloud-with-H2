4e4525833d331590b4c67fa25273f39a3924715d
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-10-22 12:29:07 +0300

    commit

--- src/main/java/com/example/tacocloudwithjpa/IngredientRepository.java
+++ src/main/java/com/example/tacocloudwithjpa/IngredientRepository.java
@@ -2,6 +2,8 @@ package com.example.tacocloudwithjpa;
 
 import data.Ingredient;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IngredientRepository extends CrudRepository<Ingredient, String> {
 }
