a4bb666a5a5ae6a645a5592ed0976aede1aaa98c
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-10-22 12:29:19 +0300

    commit

--- src/main/java/com/example/tacocloudwithjpa/OrderRepository.java
+++ src/main/java/com/example/tacocloudwithjpa/OrderRepository.java
@@ -2,6 +2,8 @@ package com.example.tacocloudwithjpa;
 
 import data.TacoOrder;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 }
