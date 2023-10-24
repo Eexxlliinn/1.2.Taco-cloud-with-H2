e0e5b7868d027731a0fbc1932d951c90b3d94609
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-10-24 20:20:44 +0300

    commit

--- src/main/java/com/example/tacocloudwithjpa/data/Taco.java
+++ src/main/java/com/example/tacocloudwithjpa/data/Taco.java
@@ -13,7 +13,6 @@ import java.util.List;
 public class Taco {
 
     @Id
-    @SequenceGenerator(name = "taco_seq", sequenceName = "taco_sequence", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "taco_seq")
     @Column(name = "id", nullable = false)
     private Long id;
