b37de15e87d90e0b00a7cec4ae31efcc2434d189
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-10-24 13:13:48 +0300

    commit

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -14,7 +14,6 @@ create table if not exists Taco (
     id identity,
     name varchar(50) not null,
     taco_order bigint not null,
-    taco_order_key bigint not null,
     created_at timestamp not null
 );
 create table if not exists Ingredient_Ref (
