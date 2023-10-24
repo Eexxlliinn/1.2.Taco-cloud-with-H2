724a22a5feca9a43792f1a942a066e18b7c9140d
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-10-24 21:21:41 +0300

    commit

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,11 @@
 drop sequence taco_sequence if exists;
 drop sequence taco_order_sequence if exists;
-create sequence taco_sequence start with 1 increment by 1;
-create sequence taco_order_sequence start with 1 increment by 1;
+create sequence taco_sequence start with 1 increment by 50;
+create sequence taco_order_sequence start with 1 increment by 50;
+drop table Taco_Order if exists cascade;
+drop table Taco if exists cascade;
+drop table Ingredient_Ref if exists cascade;
+drop table Ingredient if exists cascade;
 create table if not exists Taco_Order (
     id identity(1, 1) primary key,
     delivery_Name varchar(50) not null,
@@ -17,7 +21,7 @@ create table if not exists Taco_Order (
 create table if not exists Taco (
     id identity(1, 1) primary key,
     name varchar(50) not null,
-    taco_order bigint not null,
+    taco_order bigint,
     created_at timestamp not null
 );
 create table if not exists Ingredient_Ref (
