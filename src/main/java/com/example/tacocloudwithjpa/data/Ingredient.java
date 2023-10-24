65499743025006511aad8a8d60bea809d962de1d
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-10-24 10:22:47 +0300

    commit

--- src/main/java/com/example/tacocloudwithjpa/data/Ingredient.java
+++ src/main/java/com/example/tacocloudwithjpa/data/Ingredient.java
@@ -1,6 +1,8 @@
 package com.example.tacocloudwithjpa.data;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import lombok.*;
 
@@ -11,8 +13,12 @@ import lombok.*;
 public class Ingredient {
     @Id
     private final String id;
+
     private final String name;
+
+    @Enumerated(EnumType.STRING)
     private final Type type;
+
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
