c2f9a8baf98d9dc45d32caf346fc0da20b194b9c
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-10-25 22:35:07 +0300

    commit

--- src/main/java/com/example/tacocloudwithjpa/data/IngredientRef.java
+++ src/main/java/com/example/tacocloudwithjpa/data/IngredientRef.java
@@ -1,7 +1,12 @@
 package com.example.tacocloudwithjpa.data;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 @Data
 public class IngredientRef {
+
+    @NotNull
     private final String ingredient;
+
+    @NotNull
     private final Long taco_id;
 }
