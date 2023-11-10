9e5f02f95b880c971326dcb203d6d4006d4670b8
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-11-10 18:00:44 +0300

    commit

--- src/main/java/com/example/tacocloudwithjpa/OrderController.java
+++ src/main/java/com/example/tacocloudwithjpa/OrderController.java
@@ -4,6 +4,7 @@ import com.example.tacocloudwithjpa.repositories.OrderRepository;
 import com.example.tacocloudwithjpa.data.TacoOrder;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,7 @@ public class OrderController {
 
     private final OrderRepository orderRepo;
 
+    @Autowired
     public OrderController(OrderRepository orderRepo) {
         this.orderRepo = orderRepo;
     }
