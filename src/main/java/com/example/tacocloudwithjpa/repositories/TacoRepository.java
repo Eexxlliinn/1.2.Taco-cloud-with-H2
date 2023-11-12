69251df1580b7a53488ff3aea878629c7ef4b838
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-11-12 22:08:27 +0300

    commit

--- src/main/java/com/example/tacocloudwithjpa/repositories/TacoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.tacocloudwithjpa.repositories;
-
-import com.example.tacocloudwithjpa.data.Taco;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TacoRepository extends CrudRepository<Taco, Long> {
-}
