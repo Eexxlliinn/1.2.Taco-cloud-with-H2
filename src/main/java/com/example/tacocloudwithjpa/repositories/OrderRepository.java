c6b707b1f9964566a2049de8f679649a07a4dbc2
Eugene Yuntsevich <uncevichzena@gmail.com>
2023-11-12 20:23:20 +0300

    commit

--- src/main/java/com/example/tacocloudwithjpa/repositories/OrderRepository.java
+++ src/main/java/com/example/tacocloudwithjpa/repositories/OrderRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+    //Override save SQL Statement with insert data in taco
+    @Override
+    <S extends TacoOrder> S save(S entity);
 }
